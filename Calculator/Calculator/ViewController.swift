31096c2e26ee1bad45241c6759969e38945fb89e
WEIQIANG LIANG <WEIQIANGLIANG@WEIQIANGs-MacBook-Pro.local>
2015-12-13 12:20:29 +1100

    project init.

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by WEIQIANG LIANG on 13/12/2015.
+//  Copyright © 2015 Black Friday Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
