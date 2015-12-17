96c5ce023994a8d47844bd7ff30f002387882550
Wilkins Liang <wilkinsweiqiangliang@gmail.com>
2015-12-17 23:31:43 +1100

    Implement change from TASK1-TASK5 on assignment 1#

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -9,37 +9,73 @@
 import UIKit
 class CalculatorViewController: UIViewController {
 
+    @IBOutlet weak var history: UILabel!
     @IBOutlet weak var displayLable: UILabel!
 
     @IBOutlet var digitButton: [UIButton]!
-    var userIsInTheMiddleOfTyping:Bool = false;
 
+    // boolean value to indicate if user is not finish typing
+    var userIsInTheMiddleOfTyping:Bool = false;
+    // boolean value to indicate if decimal is used
+    var userHasUsedDecimal:Bool = false;
+    // using for π
+    let pi = M_PI;
     @IBOutlet var operationButton: [UIButton]!
     @IBAction func digitPress(sender: UIButton) {
-        let digit = sender.currentTitle;
-        if(self.userIsInTheMiddleOfTyping.boolValue){
-            displayLable.text = displayLable.text! + digit!;
-        } else {
-            displayLable.text = digit!;
-            self.userIsInTheMiddleOfTyping = true;
+        if let digit = sender.currentTitle {
+            if(digit == "." && userHasUsedDecimal){
+                // do nothing, as this is second decimal
+            } else {
+                if(digit == "."){
+                    // if user first set the decimal, we set the value to true
+                    userHasUsedDecimal = true;
+                }
+
+                if userIsInTheMiddleOfTyping {// if user is in the middle of typing, then starting should not be "0"
+                    var currentText = displayLable.text!;
+                    if currentText[currentText.startIndex] == "0" {
+                        currentText.removeAtIndex(currentText.startIndex);
+                    }
+                    displayLable.text = displayLable.text! + digit;
+                } else {
+                    if userHasUsedDecimal {// if user is not in the middle of typing and use decimal, then should starting with "0"
+                        displayLable.text = "0" + digit;
+                    } else {
+                        displayLable.text = digit;
+
+                    }
+                    
+                    self.userIsInTheMiddleOfTyping = true;
+                }
+            }
+            addToHistory(digit);
         }
     }
 
     // user hit operation key
     @IBAction func operate(sender: UIButton) {
+        if(userIsInTheMiddleOfTyping){
+            enter();
+            userIsInTheMiddleOfTyping = false;
+        }
+        
         let operation = sender.currentTitle!;
+        addToHistory(operation);
 
         switch operation {
         // passing named local parameter as op1&op2, pass inline return as the main func operation and declear what to return as Double.
         //case "+": performOperation({(op1,op2) in return op1+op2});
         case "+": performOperation { $0 + $1 };
         //case "-": performOperation({(op1,op2) in return op1-op2});
-        case "-": performOperation { $1 - $0 };
+        case "−": performOperation { $1 - $0 };
         //case "×": performOperation({(op1,op2) in return op1*op2});
         case "×": performOperation { $1 * $0 };
         //case "÷": performOperation({(op1,op2) in return op2/op1});
         case "÷": performOperation { $1 / $0 };
         case "√": performOperation { sqrt($0) };
+        case "sin": performOperation{ sin($0) };
+        case "cos": performOperation{ cos($0) };
+        case "π": displayValue = pi; enter();
         default: break;
         }
     }
@@ -63,8 +99,34 @@ class CalculatorViewController: UIViewController {
     // user hit enter key. we calling this operation as pushing result to the stack
     @IBAction func enter() {
         self.userIsInTheMiddleOfTyping = false;
+        self.userHasUsedDecimal = false;
         operandStack.append(displayValue);
         print("operandStack:"+"\(operandStack)");
+    }
+
+    @IBAction func clear() {
+        self.history.text = " ";
+        self.displayLable.text = " ";
+        self.operandStack.removeAll();
+
+    }
+
+    @IBAction func back() {
+        if userIsInTheMiddleOfTyping {
+            if displayLable.text!.characters.count > 0 {
+                // we copied the text before modified.
+                var truncatedtext = displayLable.text!;
+                truncatedtext = String(truncatedtext.characters.dropLast());
+                if truncatedtext.characters.count > 0 {
+                    self.displayLable.text = truncatedtext;
+                } else {
+                    // if user just started to type one character then we see this back to "0"
+                    self.displayLable.text = "0";
+
+                }
+            }
+        }
+        addToHistory("🔙");
     }
     var operandStack = Array<Double>();
     var displayValue:Double {
@@ -79,4 +141,13 @@ class CalculatorViewController: UIViewController {
         }
     }
 
+    // function to add operation/digit to history
+    func addToHistory(value:String) {
+        if let oldText = history.text {
+            history.text = oldText + value;
+        } else {
+            history.text = value;
+        }
+
+    }
 }
